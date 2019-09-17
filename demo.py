896a672ed044a1c06ec8545fefbafa594ce52141
Thanh Tu <tu@novalex.vn>
2019-09-17 11:16:46 +0700

    first commit

--- demo.py
+++ demo.py
@@ -4,20 +4,30 @@ import tools.utils as utils
 import tools.dataset as dataset
 from PIL import Image
 from collections import OrderedDict
+import numpy as np
 import cv2
+import glob
+import os
+import re
+import click
 from models.moran import MORAN
 
 model_path = './demo.pth'
-img_path = './demo/0.png'
-alphabet = '0:1:2:3:4:5:6:7:8:9:a:b:c:d:e:f:g:h:i:j:k:l:m:n:o:p:q:r:s:t:u:v:w:x:y:z:$'
+img_dir = '/Users/thanhtu/Projects/Python/CRAFT-pytorch/cropped/'
+alphabet = '0:1:2:3:4:5:6:7:8:9:a:b:c:d:e:f:g:h:i:j:k:l:m:n:o:p:q:r:8:t:u:v:w:x:y:z:$'
+
+target_height = 32
+target_width = 100
 
 cuda_flag = False
 if torch.cuda.is_available():
     cuda_flag = True
-    MORAN = MORAN(1, len(alphabet.split(':')), 256, 32, 100, BidirDecoder=True, CUDA=cuda_flag)
+    MORAN = MORAN(1, len(alphabet.split(':')), 256, target_height,
+                  target_width, BidirDecoder=True, CUDA=cuda_flag)
     MORAN = MORAN.cuda()
 else:
-    MORAN = MORAN(1, len(alphabet.split(':')), 256, 32, 100, BidirDecoder=True, inputDataType='torch.FloatTensor', CUDA=cuda_flag)
+    MORAN = MORAN(1, len(alphabet.split(':')), 256, target_height, target_width,
+                  BidirDecoder=True, inputDataType='torch.FloatTensor', CUDA=cuda_flag)
 
 print('loading pretrained model from %s' % model_path)
 if cuda_flag:
@@ -26,7 +36,7 @@ else:
     state_dict = torch.load(model_path, map_location='cpu')
 MORAN_state_dict_rename = OrderedDict()
 for k, v in state_dict.items():
-    name = k.replace("module.", "") # remove `module.`
+    name = k.replace("module.", "")  # remove `module.`
     MORAN_state_dict_rename[name] = v
 MORAN.load_state_dict(MORAN_state_dict_rename)
 
@@ -35,37 +45,86 @@ for p in MORAN.parameters():
 MORAN.eval()
 
 converter = utils.strLabelConverterForAttention(alphabet, ':')
-transformer = dataset.resizeNormalize((100, 32))
-image = Image.open(img_path).convert('L')
-image = transformer(image)
 
-if cuda_flag:
-    image = image.cuda()
-image = image.view(1, *image.size())
-image = Variable(image)
-text = torch.LongTensor(1 * 5)
-length = torch.IntTensor(1)
-text = Variable(text)
-length = Variable(length)
+image_paths = []
 
-max_iter = 20
-t, l = converter.encode('0'*max_iter)
-utils.loadData(text, t)
-utils.loadData(length, l)
-output = MORAN(image, length, text, text, test=True, debug=True)
+image_paths.extend(glob.glob(os.path.join(img_dir, "*.jpg")))
+found = 0
+not_found = 0
+count = 0
+ground_truth = ''
 
-preds, preds_reverse = output[0]
-demo = output[1]
 
-_, preds = preds.max(1)
-_, preds_reverse = preds_reverse.max(1)
+for img_path in image_paths:
+    count += 1
+    # if count > 23:
+    #     break
+    basename = os.path.basename(img_path)
+    # match_obj = re.search('\d*_([\d-]+)_', basename)
+    # ground_truth = match_obj.group(1)
+    image = Image.open(img_path).convert('L')
+    width, height = image.size
+    new_height = round(target_width*(height/width))
+    transformer = dataset.resizeNormalize((target_width, new_height))
+    image = transformer(image)
 
-sim_preds = converter.decode(preds.data, length.data)
-sim_preds = sim_preds.strip().split('$')[0]
-sim_preds_reverse = converter.decode(preds_reverse.data, length.data)
-sim_preds_reverse = sim_preds_reverse.strip().split('$')[0]
+    if cuda_flag:
+        image = image.cuda()
+    image = image.view(1, *image.size())
+    image = Variable(image)
+    text = torch.LongTensor(1 * 5)
+    length = torch.IntTensor(1)
+    text = Variable(text)
+    length = Variable(length)
 
-print('\nResult:\n' + 'Left to Right: ' + sim_preds + '\nRight to Left: ' + sim_preds_reverse + '\n\n')
+    max_iter = 20
+    t, l = converter.encode('0'*max_iter)
+    utils.loadData(text, t)
+    utils.loadData(length, l)
+    output = MORAN(image, length, text, text, test=True, debug=True)
 
-cv2.imshow("demo", demo)
-cv2.waitKey()
+    preds, preds_reverse = output[0]
+    demo = output[1]
+
+    _, preds = preds.max(1)
+    _, preds_reverse = preds_reverse.max(1)
+
+    sim_preds = converter.decode(preds.data, length.data)
+    sim_preds = sim_preds.strip().split('$')[0]
+    # sim_preds_reverse = converter.decode(preds_reverse.data, length.data)
+    # sim_preds_reverse = sim_preds_reverse.strip().split('$')[0]
+
+    # print('\nResult:\n' + 'Left to Right: ' + sim_preds +
+    #       '\nRight to Left: ' + sim_preds_reverse + '\n\n')
+    len_sim = len(sim_preds)
+    if len_sim == 7 or len_sim == 8:
+        sim_preds = sim_preds[0:2] + '-' + \
+            sim_preds[2:len_sim-4] + '-' + sim_preds[-4:]
+
+    if ground_truth == sim_preds:
+        found += 1
+        click.secho("{}-{}-{}".format(
+            found, sim_preds, basename), fg='green', bold=True)
+
+        open_cv_image = cv2.imread(img_path)
+        # Convert RGB to BGR
+        # open_cv_image = open_cv_image[:, :, ::-1].copy()
+        ch, cw, _ = open_cv_image.shape
+        h, w, _ = demo.shape
+        new_width = round(
+            h * (cw/ch))
+
+        open_cv_image = cv2.resize(open_cv_image, (new_width, h))
+        demo = demo[0:h, round(w/3):w]
+        demo = cv2.resize(demo, (w, h))
+        demo = cv2.hconcat([open_cv_image, demo])
+        cv2.imshow(ground_truth, demo)
+
+    else:
+        not_found += 1
+        click.secho("{}-{}-{}".format(
+            not_found, sim_preds, basename), fg='red', bold=True)
+        cv2.imshow(sim_preds, demo)
+
+
+cv2.waitKey(0)
--- models/morn.py
+++ models/morn.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from torch.autograd import Variable
 import numpy as np
 
+
 class MORN(nn.Module):
     def __init__(self, nc, targetH, targetW, inputDataType='torch.cuda.FloatTensor', maxBatch=256, CUDA=True):
         super(MORN, self).__init__()
@@ -13,13 +14,15 @@ class MORN(nn.Module):
         self.cuda = CUDA
 
         self.cnn = nn.Sequential(
-            nn.MaxPool2d(2, 2), 
-            nn.Conv2d(nc, 64, 3, 1, 1), nn.BatchNorm2d(64), nn.ReLU(True), nn.MaxPool2d(2, 2),
-            nn.Conv2d(64, 128, 3, 1, 1), nn.BatchNorm2d(128), nn.ReLU(True), nn.MaxPool2d(2, 2),
-            nn.Conv2d(128, 64, 3, 1, 1), nn.BatchNorm2d(64), nn.ReLU(True), 
-            nn.Conv2d(64, 16, 3, 1, 1), nn.BatchNorm2d(16), nn.ReLU(True), 
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(nc, 64, 3, 1, 1), nn.BatchNorm2d(
+                64), nn.ReLU(True), nn.MaxPool2d(2, 2),
+            nn.Conv2d(64, 128, 3, 1, 1), nn.BatchNorm2d(
+                128), nn.ReLU(True), nn.MaxPool2d(2, 2),
+            nn.Conv2d(128, 64, 3, 1, 1), nn.BatchNorm2d(64), nn.ReLU(True),
+            nn.Conv2d(64, 16, 3, 1, 1), nn.BatchNorm2d(16), nn.ReLU(True),
             nn.Conv2d(16, 1, 3, 1, 1), nn.BatchNorm2d(1)
-            )
+        )
 
         self.pool = nn.MaxPool2d(2, 1)
 
@@ -27,10 +30,10 @@ class MORN(nn.Module):
         w_list = np.arange(self.targetW)*2./(self.targetW-1)-1
 
         grid = np.meshgrid(
-                w_list, 
-                h_list, 
-                indexing='ij'
-            )
+            w_list,
+            h_list,
+            indexing='ij'
+        )
         grid = np.stack(grid, axis=-1)
         grid = np.transpose(grid, (1, 0, 2))
         grid = np.expand_dims(grid, 0)
@@ -38,7 +41,7 @@ class MORN(nn.Module):
         grid = torch.from_numpy(grid).type(self.inputDataType)
         if self.cuda:
             grid = grid.cuda()
-            
+
         self.grid = Variable(grid, requires_grad=False)
         self.grid_x = self.grid[:, :, :, 0].unsqueeze(3)
         self.grid_y = self.grid[:, :, :, 1].unsqueeze(3)
@@ -46,7 +49,7 @@ class MORN(nn.Module):
     def forward(self, x, test, enhance=1, debug=False):
 
         if not test and np.random.random() > 0.5:
-            return nn.functional.upsample(x, size=(self.targetH, self.targetW), mode='bilinear')
+            return nn.functional.interpolate(x, size=(self.targetH, self.targetW), mode='bilinear', align_corners=True)
         if not test:
             enhance = 0
 
@@ -56,7 +59,8 @@ class MORN(nn.Module):
         grid = self.grid[:x.size(0)]
         grid_x = self.grid_x[:x.size(0)]
         grid_y = self.grid_y[:x.size(0)]
-        x_small = nn.functional.upsample(x, size=(self.targetH, self.targetW), mode='bilinear')
+        x_small = nn.functional.interpolate(
+            x, size=(self.targetH, self.targetW), mode='bilinear', align_corners=True)
 
         offsets = self.cnn(x_small)
         offsets_posi = nn.functional.relu(offsets, inplace=False)
@@ -75,7 +79,8 @@ class MORN(nn.Module):
             offsets_nega = nn.functional.relu(-offsets, inplace=False)
             offsets_pool = self.pool(offsets_posi) - self.pool(offsets_nega)
 
-            offsets_grid += nn.functional.grid_sample(offsets_pool, grid).permute(0, 2, 3, 1).contiguous()
+            offsets_grid += nn.functional.grid_sample(
+                offsets_pool, grid).permute(0, 2, 3, 1).contiguous()
             offsets_x = torch.cat([grid_x, grid_y + offsets_grid], 3)
             x_rectified = nn.functional.grid_sample(x, offsets_x)
 
@@ -96,7 +101,8 @@ class MORN(nn.Module):
             cmap = plt.get_cmap("rainbow")
             blue = Color("blue")
             hex_colors = list(blue.range_to(Color("red"), density_range))
-            rgb_colors = [[rgb * 255 for rgb in color.rgb] for color in hex_colors][::-1]
+            rgb_colors = [[rgb * 255 for rgb in color.rgb]
+                          for color in hex_colors][::-1]
             to_pil_image = transforms.ToPILImage()
 
             for i in range(x.size(0)):
@@ -110,15 +116,19 @@ class MORN(nn.Module):
 
                 v_max = offsets_max.data[i]
                 v_min = offsets_min.data[i]
-                img_offsets = (offsets_grid[i]).view(1, self.targetH, self.targetW).data.cpu().add_(-v_min).mul_(1./(v_max-v_min))
+                img_offsets = (offsets_grid[i]).view(
+                    1, self.targetH, self.targetW).data.cpu().add_(-v_min).mul_(1./(v_max-v_min))
                 img_offsets = to_pil_image(img_offsets)
                 img_offsets = np.array(img_offsets)
-                color_map = np.empty([self.targetH, self.targetW, 3], dtype=int)
+                color_map = np.empty(
+                    [self.targetH, self.targetW, 3], dtype=int)
                 for h_i in range(self.targetH):
                     for w_i in range(self.targetW):
-                        color_map[h_i][w_i] = rgb_colors[int(img_offsets[h_i, w_i]/256.*density_range)]
+                        color_map[h_i][w_i] = rgb_colors[int(
+                            img_offsets[h_i, w_i]/256.*density_range)]
                 color_map = color_map.astype(np.uint8)
-                cv2.addWeighted(color_map, alpha, img_copy, 1-alpha, 0, img_copy)
+                cv2.addWeighted(color_map, alpha, img_copy,
+                                1-alpha, 0, img_copy)
 
                 img_processed = x_rectified[i].data.cpu().mul_(0.5).add_(0.5)
                 img_processed = to_pil_image(img_processed)
@@ -126,10 +136,13 @@ class MORN(nn.Module):
                 if len(img_processed.shape) == 2:
                     img_processed = cv2.merge([img_processed.copy()]*3)
 
-                total_img = np.ones([self.targetH, self.targetW*3+10, 3], dtype=int)*255
+                total_img = np.ones(
+                    [self.targetH, self.targetW*3+10, 3], dtype=int)*255
                 total_img[0:self.targetH, 0:self.targetW] = img
-                total_img[0:self.targetH, self.targetW+5:2*self.targetW+5] = img_copy
-                total_img[0:self.targetH, self.targetW*2+10:3*self.targetW+10] = img_processed
+                total_img[0:self.targetH, self.targetW +
+                          5:2*self.targetW+5] = img_copy
+                total_img[0:self.targetH, self.targetW *
+                          2+10:3*self.targetW+10] = img_processed
                 total_img = cv2.resize(total_img.astype(np.uint8), (300, 50))
                 # cv2.imshow("Input_Offsets_Output", total_img)
                 # cv2.waitKey()
